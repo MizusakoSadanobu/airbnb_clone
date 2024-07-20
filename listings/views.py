7e40c4701e9ef1186e5089ca5b7033157e989439
sadanobu.mizusako <sadanobu.mizusako@gmail.com>
2024-07-20 17:52:03 +0900

    added basic codes

---
+++ listings/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Listing
+
+def listing_list(request):
+    listings = Listing.objects.all()
+    return render(request, 'listings/listing_list.html', {'listings': listings})
